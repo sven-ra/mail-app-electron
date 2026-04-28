fd2421c6b5e29c4db244aca9c2163057f57aa6d0
sven-ra <mail@svenraju.net>
2026-04-28 16:58:31 +0300

    Add Vite dev server support for Electron development
    - main.js now loads http://localhost:5173 in development mode
    - Added 'dev' and 'watch' scripts using concurrently to run Vite + Electron together
    - Added concurrently dependency for parallel process management
    - Production still loads dist/index.html as before

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ function createWindow() {
     },
   });
 
-  mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
 }
 
 app.whenReady().then(createWindow);
